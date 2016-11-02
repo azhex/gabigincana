18824e27eedb8d7087cb7d693b0607748770b4c4
Gabriel Revilla Horcas <azhex99@gmail.com>
2016-11-02 15:54:33 +0100

    Update Server.java

--- GabiGincana-Server/src/gabigincana/Server.java
+++ GabiGincana-Server/src/gabigincana/Server.java
@@ -10,18 +10,38 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Date;
 
+import javax.swing.JOptionPane;
+
 public class Server {
 	public ServerSocket serverSocket;
 	public Socket[] clientsSocket;
+	public String[] cliUser;
+	public boolean[] cliPlaying;
+	public int[] cliScore;
 	public int maxClients;
+	public String gincanaClave = "";
+	public boolean conectado;
+	private String separador1 = "-1-2-3-4-";
+	private GUI gui;
 	
-	public Server(int port, int mC) {
+	public Server(int port, int mC, GUI g) {
 		try{
 			maxClients = mC;
+			gui = g;
 			clientsSocket = new Socket[maxClients];
-			serverSocket = new ServerSocket(port);
+			cliUser = new String[maxClients];
+			cliScore = new int[maxClients];
+			cliPlaying = new boolean[maxClients];
 			
+			gincanaClave = JOptionPane.showInputDialog(gui.frm, "Introduce una clave para la gincana, cuando un usuario entre tendra que introducirla");
+			if(gincanaClave == null || gincanaClave.equals("")){
+				gincanaClave = "000000";
+			}
+			
+			serverSocket = new ServerSocket(port);
+			conectado = true;
 			System.out.println("[GabiGincana] Servidor iniciado puerto: " + String.valueOf(port));
+			gui.printLog("[Servidor iniciado puerto: " + String.valueOf(port) + " | Clave: " + gincanaClave + "]" + "\n");
 			
 			new Thread(new Runnable(){
 				public void run(){
@@ -48,15 +68,15 @@ public class Server {
 			new Thread(new Runnable(){
 				@Override
 				public void run() {
-					System.out.println("[GabiGincana] Enviando informacion de procesamiento requerida a los clientes, intervalo de 5 seg [OK]");
-					while(true){
+					System.out.println("[GabiGincana] Enviando informacion de procesamiento requerida a los clientes, intervalo de 2 seg [OK]");
+					while(conectado){
 						for(int i=0;i < maxClients; i++){
 							if(clientsSocket[i] != null && clientsSocket[i].isConnected()){
-								sendClientList(clientsSocket[i], String.valueOf(i));
+								sendRequiredData(clientsSocket[i], i);
 							}
 						}
 						try {
-							Thread.sleep(5000);
+							Thread.sleep(1000);
 						} catch (InterruptedException e) {
 							e.printStackTrace();
 						}
@@ -66,65 +86,62 @@ public class Server {
 			
 		} catch (IOException e1) {
 			System.out.println("[GabiGincana] Error al iniciar servidor [" + e1.toString() + "]");
+			gui.printLog("[Error al iniciar servidor [" + e1.toString() + "]]" + "\n");
 		}
 	}
 	
 	public void closeConnections() throws IOException{
 		for(int i=0;i<clientsSocket.length;i++){
 			if(clientsSocket[i] != null){
-				clientsSocket[i].close();
-				clientsSocket[i] = null;
+				cerrarConexion(i);
 			}
 		}
+		serverSocket.close();
+		conectado = false;
+		
+		gui.printLog("[Servidor cerrado]" + "\n");
 	}
 	
 	private void waitConnection() throws IOException{
-		while(true){
+		while(conectado){
 			for(int i=0;i < maxClients; i++){
 				if(clientsSocket[i] == null || clientsSocket[i].isInputShutdown()){
 					final int clientN = i;
 					clientsSocket[i] = serverSocket.accept();
 					if(clientsSocket[i].isConnected()){
 						System.out.println("Cliente conectado -> "+i);
-						PrintWriter out = new PrintWriter(clientsSocket[i].getOutputStream(), true);
+						gui.printLog("[Cliente conectado -> " + String.valueOf(i) + "]" + "\n");
 						new Thread(new Runnable(){
 							public void run(){
 								recibirMensajes(clientN);
 							}
 						}).start();
-						System.out.println("[Mensaje de bienvenida a "+i+"]");
-						out.println("[Bienvenido al servidor de azhex, eres el cliente -> "+i+"]");
+						
 						break;
 					}
-				}else{
-					
 				}
 			}
 		}
 	}
 	
-	private void sendClientList(Socket cli, String id){
-		String connMangData = "";
+	private void sendRequiredData(Socket cli, int id){
+		int top = cliScore.length;
 		
-		connMangData += "clientslist-1-2-3-";
-		connMangData += new Date()+"-4-5-6-";
-		connMangData += "------------Connection Manager-------------4-5-6-";
-		for(int i=0;i < maxClients;i++){
-			if(clientsSocket[i] == null){
-				connMangData += String.valueOf(i)+" -> null-4-5-6-";
-			}else if(clientsSocket[i].isInputShutdown() || clientsSocket[i].isClosed() || clientsSocket[i].isOutputShutdown()){
-				connMangData += String.valueOf(i)+" -> Desconectado-4-5-6-";
-				clientsSocket[i] = null;
-			}else{
-				connMangData += String.valueOf(i)+" -> ["+clientsSocket[i].getInetAddress().toString()+":"+String.valueOf(clientsSocket[i].getPort())+"] [OK]-4-5-6-";
+		for(int i=0;i < cliScore.length;i++){
+			if(cliScore[i] < cliScore[id]){
+				top--;
 			}
 		}
-		connMangData += "-------------------------------------------4-5-6-";
-		sendMsg(cli, connMangData, id, false);
+		
+		cliScore[id] += 10;
+		
+		sendMsg(cli, "userInfo" + separador1 + String.valueOf(cliScore[id]) + separador1 + String.valueOf(top));
+		
+		return;
 	}
 	
 	private void manageConnections() throws IOException, InterruptedException{
-		while(true){
+		while(conectado){
 			String connMangData = "";
 			
 			connMangData += new Date()+"\n";
@@ -132,21 +149,23 @@ public class Server {
 			for(int i=0;i < maxClients;i++){
 				if(clientsSocket[i] == null){
 					connMangData += String.valueOf(i)+" -> null\n";
+					cliUser[i] = "";
 				}else if(clientsSocket[i].isInputShutdown() || clientsSocket[i].isClosed() || clientsSocket[i].isOutputShutdown()){
 					connMangData += String.valueOf(i)+" -> Desconectado\n";
-					clientsSocket[i] = null;
+					cerrarConexion(i);
 				}else{
-					connMangData += String.valueOf(i)+" -> ["+clientsSocket[i].getInetAddress().toString()+":"+String.valueOf(clientsSocket[i].getPort())+"] [OK]\n";
+					connMangData += String.valueOf(i)+" -> ["+cliUser[i]+"] ["+clientsSocket[i].getInetAddress().toString()+"] [OK]\n";
 				}
 			}
 			connMangData += "------------------------------------------\n";
+			gui.printClients(connMangData);
 			
 			Thread.sleep(1000);
 		}
 	}
 	
 	private void recibirMensajes(int i){
-		while(true){
+		while(conectado){
 			if(clientsSocket[i] != null && !clientsSocket[i].isInputShutdown() && !clientsSocket[i].isClosed()){
 				try {
 					String data;
@@ -155,33 +174,68 @@ public class Server {
 					data = in.readLine();
 					
 					if(data == null){
-						clientsSocket[i].close();
-						clientsSocket[i] = null;
+						cerrarConexion(i);
 						break;
 					}else{
-						String[] dataAux = data.split("-1-2-3-");
-						
-						/*
-						 * DataAux -> Datos extra que deben ser procesados
-						 * Chat -> DataAux[0]=tipo de procesamiento, dataAux[1]:mensaje, dataAux[2]:client id
-						 */
+						String[] dataAux = data.split(separador1);
 						
 						if(dataAux.length > 1){
-							if(dataAux.length == 2){
-								if(dataAux[0].equals("sendall")){
-									sendMsgAll(clientsSocket, "chatrecv-1-2-3-"+String.valueOf(i)+"-1-2-3-"+dataAux[1], false);
+							if(dataAux.length == 3){
+								if(dataAux[0].equals("login")){
+									boolean repetido = false;
+									for(int u=0;u<cliUser.length;u++){
+										if(cliUser[u].equals(dataAux[1])){
+											repetido = true;
+											break;
+										}
+									}
+									if(dataAux[1].toUpperCase().matches(".*SERVIDOR.*")){
+										repetido = true;
+									}
+									if(dataAux[2].equals(gincanaClave)){
+										if(!repetido){
+											cliUser[i] = dataAux[1];
+											sendMsg(clientsSocket[i], 
+													"login" + separador1 +
+													"OK");
+											gui.printLog("[Login] IP: " + clientsSocket[i].getInetAddress() + " | USUARIO: " + dataAux[1] + " | CLAVE: " + dataAux[2] + "\n");
+											System.out.println("Login desde " + clientsSocket[i].getInetAddress() + " user: " + dataAux[1] + " clave: " + dataAux[2]);
+										}else{
+											cliUser[i] = dataAux[1];
+											sendMsg(clientsSocket[i], "USER_EXISTENTE");
+											gui.printLog("[LoginError][USER_EXISTENTE] IP: " + clientsSocket[i].getInetAddress() + " | USUARIO: " + dataAux[1] + " | CLAVE: " + dataAux[2] + "\n");
+											System.out.println("Login error [USER_EXISTENTE] desde " + clientsSocket[i].getInetAddress() + " user: " + dataAux[1] + " clave: " + dataAux[2]);
+										}
+									}else{
+										cliUser[i] = dataAux[1];
+										sendMsg(clientsSocket[i], "CLAVE_INCORRECTA");
+										gui.printLog("[LoginError][CLAVE_INCORRECTA] IP: " + clientsSocket[i].getInetAddress() + " | USUARIO: " + dataAux[1] + " | CLAVE: " + dataAux[2] + "\n");
+										System.out.println("Login error [CLAVE_INCORRECTA] desde " + clientsSocket[i].getInetAddress() + " user: " + dataAux[1] + " clave: " + dataAux[2]);
+									}
+								}else if(dataAux[0].equals("chatMsgSend")){
+									sendMsgAll(clientsSocket, "chatMsgRecv"+ separador1 + "<b>" + dataAux[1] + "</b>" + separador1 + dataAux[2]);
+									gui.printLog("[ChatMsg] Mensaje de [" + dataAux[1] + "] -> " + dataAux[2] + "\n");
 								}
-							}else if(dataAux.length == 3){
-								if(dataAux[0].equals("sendto")){
-									sendMsg(clientsSocket[Integer.parseInt(dataAux[2].toString())], "chatrecv-1-2-3-"+String.valueOf(i)+"-1-2-3-"+dataAux[1], dataAux[2].toString(), false);
+							}else if(dataAux.length == 2){
+								if(dataAux[0].equals("playing")){
+									if(dataAux[1].equals("OK")){
+										cliPlaying[i] = true;
+										sendMsgAll(clientsSocket, "chatMsgRecv" + separador1 + "<b><i><font color=\"#FA5858\">[SERVIDOR]" + separador1 + "[" + cliUser[i] + " ha entrado en el chat]</font></i></b>");
+										gui.printLog("[ChatInfo] El usuario [" + cliUser[i] + "] ha entrado al chat\n");
+									}else if(dataAux[1].equals("CLOSE")){
+										cliPlaying[i] = false;
+									}
 								}
 							}else{
+								System.out.println("MSG: " + data);
 							}
 						}else{
+							System.out.println("MSG: " + data);
 						}
 					}
 				} catch (IOException e1) {
 					System.out.println("Connection reset error en recv");
+					cerrarConexion(i);
 				}
 			}else{
 				break;
@@ -189,7 +243,7 @@ public class Server {
 		}
 	}
 	
-	private void sendMsgAll(Socket[] clientsSocket, String msg, boolean display){
+	private void sendMsgAll(Socket[] clientsSocket, String msg){
 		for(int i = 0;i < clientsSocket.length;i++){
 			if(clientsSocket[i] != null){
 				try {
@@ -202,7 +256,7 @@ public class Server {
 		}
 	}
 	
-	private void sendMsg(Socket clientSocket, String msg, String id, boolean display){
+	private void sendMsg(Socket clientSocket, String msg){
 		if(clientSocket != null){
 			try {
 				PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
@@ -211,5 +265,20 @@ public class Server {
 				e1.printStackTrace();
 			}
 		}
+	}
+	
+	private void cerrarConexion(int id){
+		if(clientsSocket[id] != null){
+			try {
+				clientsSocket[id].close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		clientsSocket[id] = null;
+		cliUser[id] = "";
+		cliScore[id] = 0; 
+		cliPlaying[id] = false;
 	}
 }
